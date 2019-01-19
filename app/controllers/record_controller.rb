ba11c803e58f523cd40a01371c2c80dfb50f2890
jdpp95 <jdporrasp@unal.edu.co>
2019-01-18 21:49:44 -0500

    Remove duplicates

--- app/controllers/record_controller.rb
+++ app/controllers/record_controller.rb
@@ -1,2 +1,16 @@
 class RecordController < ApplicationController
+	def remove_duplicates
+		@location = params[:location]
+		records = Record.all.order(:station_id, :time)
+		previous = nil
+		records.each do |r|
+			puts r.time.to_s + ", " + r.station_id.to_s
+			if(previous == r)
+				previous = r
+				r.delete
+			end
+			previous = r
+		end
+		redirect_to root_path
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get '/unavailable', to: 'locations#unavailable', as:'service_unavailable'
   get '/locations/:id/update-weather', to: 'locations#update_weather_data', as: 'update_weather_data'
   post '/stations/:id/update-temperature', to: 'stations#update_temperature', as: 'update_station_temperature'
+  get '/remove_duplicates', to: 'record#remove_duplicates'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
